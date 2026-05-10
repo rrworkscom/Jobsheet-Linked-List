8718617b748fb28d2a6d85d0a813c890a19525c0
rrworkscom <rrworks010@gmail.com>
2026-05-10 22:44:19 +0700

    Linked List Experiment I - Node

---
+++ week12/Node.java
@@ -0,0 +1,14 @@
+package week12;
+
+public class Node {
+    Student data;
+    Node next;
+
+    public Node() {
+    }
+
+    public Node(Student data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
