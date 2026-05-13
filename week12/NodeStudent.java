e5dfd191bb28d5e7db2ebab8a8c2202d69204523
rrworkscom <rrworks010@gmail.com>
2026-05-13 22:27:16 +0700

    Task - Node Student

---
+++ week12/NodeStudent.java
@@ -0,0 +1,11 @@
+package week12;
+public class NodeStudent {
+    StudentLL data;
+    NodeStudent next;
+
+    public NodeStudent(StudentLL data, NodeStudent next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
